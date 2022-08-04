764a48f726b1c8e9fa8a0d9abcac1f09b7a0734e
Lauren Escobedo <lescobedo4@mail.stmarytx.edu>
2022-08-04 02:00:29 -0500

    Adding Instructions to Problem 3

--- fuel.py
+++ fuel.py
@@ -1,11 +1,17 @@
 def fuel_level():
     while 1:
-        try:
-            num = user_input("numerator")
-            denom = user_input("denominator")
-            return float(num / denom)
-        except ZeroDivisionError:
+        num = user_input("numerator")
+        denom = user_input("denominator")
+        if denom == 0:
             print("Cannot divide by zero!")
+            pass
+        elif num < 0 or denom < 0:
+            print("Integers must be positive")
+            pass
+        elif num > denom:
+            print("Numerator must be smaller than denominator!")
+        else:
+            return float(num / denom)
 
 
 def user_input(name):
@@ -33,3 +39,52 @@ def main():
 
 
 main()
+
+## End Program ##
+
+#####################
+#   Instructions    #
+#####################
+
+# Fuel gauges indicate, often with fractions, just how much fuel is in a tank.
+# For instance 1/4 indicates that a tank is 25% full,
+# 1/2 indicates that a tank is 50% full, and 3/4 indicates that a tank is 75% full.
+#
+# In a file called fuel.py, implement a program that prompts the user for a fraction,
+# formatted as X/Y, wherein each of X and Y is an integer, and then outputs,
+# as a percentage rounded to the nearest integer, how much fuel is in the tank.
+# If, though, 1% or less remains, output E instead to indicate that the tank is
+# essentially empty. And if 99% or more remains, output F instead to indicate that
+# the tank is essentially full.
+#
+# If, though, X or Y is not an integer, X is greater than Y, or Y is 0,
+# instead prompt the user again. (It is not necessary for Y to be 4.)
+# Be sure to catch any exceptions like ValueError or ZeroDivisionError.
+
+
+####################
+#   How to Test    #
+####################
+
+# Here’s how to test your code manually:
+#
+#     Run your program with python fuel.py. Type 3/4 and press Enter. Your program should output:
+#
+#     75%
+#
+#     Run your program with python fuel.py. Type 1/4 and press Enter. Your program should output:
+#
+#     25%
+#
+#     Run your program with python fuel.py. Type 4/4 and press Enter. Your program should output:
+#
+#     F
+#
+#     Run your program with python fuel.py. Type 0/4 and press Enter. Your program should output:
+#
+#     E
+#
+#     Run your program with python fuel.py. Type 4/0 and press Enter. Your program should handle a ZeroDivisionError and prompt the user again.
+#     Run your program with python fuel.py. Type three/four and press Enter. Your program should handle a ValueError and prompt the user again.
+#     Run your program with python fuel.py. Type 1.5/3 and press Enter. Your program should handle a ValueError and prompt the user again.
+#     Run your program with python fuel.py. Type 5/4 and press Enter. Your program should prompt the user again.
